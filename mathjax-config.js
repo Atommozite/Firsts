bc31744cc1d322e88352c31bbd40a5a62d5f96a8
Tomas L. Howson <tomas.howson@adelaide.edu.au>
2020-02-03 16:23:56 +1030

    adding mathjax config file

---
+++ mathjax-config.js
@@ -0,0 +1,6 @@
+window.MathJax = {
+  loader: {load: ['[tex]/physics']},
+  tex: {packages: {'[+]': ['physics']}}
+};
+
+Mathjax.Ajax.loadComplete("https://raw.github.cs.adelaide.edu.au/a1686573/s4s-wiki/gh-pages/mathjax-config.js");
