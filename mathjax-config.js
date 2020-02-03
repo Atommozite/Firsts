3db2a1a9a07c4620eaed4d1fe5161789f5f5c35c
Tomas L. Howson <tomas.howson@adelaide.edu.au>
2020-02-03 16:37:41 +1030

    adding mathjax config file

--- mathjax-config.js
+++ mathjax-config.js
@@ -3,4 +3,11 @@ window.MathJax = {
   tex: {packages: {'[+]': ['physics']}}
 };
 
-Mathjax.Ajax.loadComplete("https://raw.githubusercontent.com/Atommozite/atomic-vault/master/mathjax-config.js");
+(function () {
+  var script = document.createElement('script');
+  script.src = 'https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-svg.js';
+  script.async = true;
+  document.head.appendChild(script);
+})();
+
+// Mathjax.Ajax.loadComplete("https://raw.githubusercontent.com/Atommozite/atomic-vault/master/mathjax-config.js");
